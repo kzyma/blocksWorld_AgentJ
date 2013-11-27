12d6767d7e29a4ebecd1c6b8dd2927a2f9158864
Erin <efowler@gmail.com>
2013-11-27 14:15:06 -0500

    Update createWorld_BW.py
    fixed line 230 as it was generating an error when ran as this:
    args = eval(nodesAndRelsN2J)
    in addition commented out lines 228 and 242

--- createWorld_BW.py
+++ createWorld_BW.py
@@ -225,9 +225,9 @@ def generateBW(n):
         #convert to string and remove first and last element (the " and ")
         #-->>this is nessesary to do graph.db.create(nodesAndRelsN2J)
         #str(nodesAndRelsN2J)[1:-1]
-<<<<<<< HEAD
+#<<<<<<< HEAD
         nodesAndRelsN2J = nodesN2J + relsN2J
-        args = eval(nodesAndRelsN2J)
+        args = str(nodesAndRelsN2J).strip('[]')
 =======
         tempRels = []
         for i in relsN2J:
@@ -239,7 +239,7 @@ def generateBW(n):
         i= eval(str(nodesAndRelsN2J).strip('[]'))
         graph_db.create(*i)
 
->>>>>>> versionII
+#>>>>>>> versionII
 
 # *NOTE* args = (arg1, arg2, arg3)
 #                func(*args)
